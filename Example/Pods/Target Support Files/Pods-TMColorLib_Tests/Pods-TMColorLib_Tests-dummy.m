9e04fe44ea64a89c073adf750a8cf76d056878bd
iOSMonk <898300117@qq.com>
2019-06-14 14:42:41 +0800

    init library

--- Example/Pods/Target Support Files/Pods-TMColorLib_Tests/Pods-TMColorLib_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TMColorLib_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_TMColorLib_Tests
-@end
--- Example/Pods/Target Support Files/TMColorLib/TMColorLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TMColorLib : NSObject
-@end
-@implementation PodsDummy_TMColorLib
-@end
